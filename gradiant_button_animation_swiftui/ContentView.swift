9b79311e86f3b8ebc1eff543f8df28c34b90ccf3
Obada Alhalabi <abadah.alhalabi2020@gmail.com>
2024-07-17 11:37:39 +0400

    Initial Commit

---
+++ gradiant_button_animation_swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gradiant_button_animation_swiftui
+//
+//  Created by Obada Alhalabi on 17/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gradiant_button_animation_swiftui/gradiant_button_animation_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  gradiant_button_animation_swiftuiApp.swift
+//  gradiant_button_animation_swiftui
+//
+//  Created by Obada Alhalabi on 17/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct gradiant_button_animation_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
