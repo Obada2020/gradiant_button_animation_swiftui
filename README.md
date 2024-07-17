6f543965c16504a8b4e3f98b5e5bd7f1ad5da0eb
Obada Alhalabi <abadah.alhalabi2020@gmail.com>
2024-07-17 12:13:56 +0400

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # gradiant_button_animation_swiftui
+
+<p align='center'>
+    <img src='https://raw.githubusercontent.com/Obada2020/gradiant_button_animation_swiftui/main/preview.gif'/>
+</p>
--- gradiant_button_animation_swiftui/ContentView.swift
+++ gradiant_button_animation_swiftui/ContentView.swift
@@ -1,23 +1,50 @@
-//
-//  ContentView.swift
-//  gradiant_button_animation_swiftui
-//
-//  Created by Obada Alhalabi on 17/07/2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button(action: {}, label: {
+                Text("Obada Alhalabi")
+            }).buttonStyle(GradientButtonStyle())
         }
-        .padding()
+       
     }
 }
+
+struct GradientButtonStyle : ButtonStyle{
+    
+    let gredientColors = Gradient(colors: [.red, .orange, .yellow , .green, .blue, .purple , .pink])
+    
+    @State var isAnimating = false
+    @State var test = false
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack{
+            configuration.label.font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
+                .foregroundStyle(.white)
+                .frame(width: 300, height: 80)
+                .background(.black , in: .rect(cornerRadius: 10))
+                .overlay(){ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(AngularGradient.init(gradient: gredientColors, center: .center, angle: .degrees(isAnimating ? 360 : 0)), lineWidth: 6)
+                }}
+                .scaleEffect(test ? 1.1 : 1)
+                            
+        }
+        .onAppear(){
+            withAnimation(.linear(duration: 2).repeatForever(autoreverses: false)){
+                isAnimating = true
+            }
+            //
+            withAnimation(.linear(duration: 2).repeatForever(autoreverses: true)){
+                self.test.toggle()
+            }
+            
+        }
+        
+    }
+    
+}
+
 
 #Preview {
     ContentView()
